015be888d2bc16874f2b74cf534908929b4cf4d9
slofuno <slofurno@gmail.com>
2015-03-09 00:50:40 -0400

    clean up

--- pptParse/Program.cs
+++ pptParse/Program.cs
@@ -19,19 +19,28 @@ namespace pptParse
     static void Main(string[] args)
     {
 
+
       var liscense = File.OpenRead("Aspose.Total.lic");
       var mylicense = new Aspose.Slides.License();
       mylicense.SetLicense(liscense);
 
-      var fs = File.Open("4.pptx", FileMode.Open);
+      var fs = File.Open(args[0], FileMode.Open);
 
-      var pres = new Presentation(fs);
+      var presentation = new Presentation(fs);
 
-      var slides = pres.Slides;
+      ParseDocument(presentation);
+      ProcessText(presentation);
 
-      var sw = new Stopwatch();
-      sw.Start();
+      
 
+
+    }
+
+    static void ProcessText(Presentation presentation)
+    {
+
+      var slides = presentation.Slides;
+      
       var presentationdata = new List<SlideMetaData>();
 
       foreach (var slide in slides)
@@ -42,54 +51,6 @@ namespace pptParse
         CollectText(slide.Shapes, slidedata);
 
         presentationdata.Add(slidedata);
-
-        /*
-        foreach (var shape in slide.Shapes)
-        {
-
-          if (!shape.IsTextHolder)
-          {
-           // continue;
-          }
-          
-          if (shape.GetType().Equals(typeof(GroupShape)))
-          {
-            Debug.WriteLine(shape.GetType().Name);
-          }
-          
-          var textshape = shape as AutoShape;
-
-          if (textshape == null)
-          {
-            continue;
-          }
-
-          var innertext = textshape.TextFrame.Text;
-          string title = null;
-          var text = new List<string>();
-
-
-          if (shape.Name.IndexOf("title", StringComparison.OrdinalIgnoreCase) >= 0)
-          {
-
-            if (title != null)
-            {
-              var thos = "maybe";
-
-            }
-
-            title = innertext;
-          }
-          else
-          {
-            text.Add(innertext);
-          }
-        
-
-
-
-        }
-        */
 
       }
 
@@ -139,26 +100,21 @@ namespace pptParse
 
       }
 
-      sw.Stop();
-      Debug.WriteLine("elapsed: " + sw.Elapsed.TotalMilliseconds);
-
-      
-
+     
       while (true)
       {
         Console.WriteLine("enter a key to search on: ");
         var input = Console.ReadLine().ToLowerInvariant();
+              
 
-        sw.Restart();
-
-        List<Tuple<int,int, int>> result;
+        List<Tuple<int, int, int>> result;
 
         if (!wordkey.TryGetValue(input, out result))
         {
           continue;
         }
 
-                
+
         foreach (var text in result)
         {
 
@@ -166,32 +122,16 @@ namespace pptParse
           var textindex = text.Item2;
           var wordindex = text.Item3;
 
-          //Debug.WriteLine("word: " + input + ", slide: " + slideindex + ", line: " + textindex + ", word: " + wordindex);
-
           var line = presentationdata[slideindex].text[textindex];
-          //var words = presentationdata[slideindex].text[textindex].Split('\x20');
+
 
           var min = Math.Max(wordindex - 25, 0);
-          var max = Math.Min(line.Length-1, wordindex + 25);
+          var max = Math.Min(line.Length - 1, wordindex + 25);
 
           var context = line.Substring(min, max - min);
-          /*
-          for (var i = min; i < max; i++)
-          {
-            if (i>0){
-              context += " ";
-            }
-
-            context += words[i];
-          }*/
-
-          
-          //Console.WriteLine(context);
 
         }
-
-        sw.Stop();
-        Debug.WriteLine("results: " + result.Count + ", search time : " + sw.Elapsed.TotalMilliseconds);
+               
 
       }
 
@@ -211,14 +151,11 @@ namespace pptParse
 
         if (shape.GetType().Equals(typeof(GroupShape)))
         {
-          Debug.WriteLine(shape.GetType().Name);
-
+          
           var groupshape = shape as GroupShape;
-
           CollectText(groupshape.Shapes, slidedata);
 
           continue;
-
         }
 
         var textshape = shape as AutoShape;
@@ -229,55 +166,24 @@ namespace pptParse
         }
 
         var innertext = textshape.TextFrame.Text;
-      
-       // var text = new List<string>();
 
 
-        if (shape.Name.IndexOf("title", StringComparison.OrdinalIgnoreCase) >= 0)
-        {
-
-          if (slidedata.title != null)
-          {
-
-            var issue = true;
-          }
-          else
-          {
-
-            slidedata.title = innertext;
-          }
+        if (slidedata.title == null && shape.Name.IndexOf("title", StringComparison.OrdinalIgnoreCase) >= 0)
+        {        
+          slidedata.title = innertext;          
         }
         else
         {
           slidedata.text.Add(innertext);
         }
-
-
-
       }
-
-
     }
 
-
-    static void ParseDocument()
+    static void ParseDocument(Presentation presentation)
     {
 
-      //Console.WriteLine("opening " + args[0]);
-      var liscense = File.OpenRead("Aspose.Total.lic");
-      var mylicense = new Aspose.Slides.License();
-      mylicense.SetLicense(liscense);
 
-      Aspose.Slides.Export.PdfOptions PdfOptions = new Aspose.Slides.Export.PdfOptions();
-      PdfOptions.SufficientResolution = 800;
-      PdfOptions.SaveMetafilesAsPng = true;
-      PdfOptions.JpegQuality = 100;
-
-      var fs = File.Open("4.pptx", FileMode.Open);
-
-      var pres = new Presentation(fs);
-
-      var slides = pres.Slides;
+      var slides = presentation.Slides;
       var size = new System.Drawing.Size(960, 720);
 
       for (var i = 0; i < slides.Count; i++)
@@ -293,33 +199,34 @@ namespace pptParse
         }
 
 
-        var ms = new MemoryStream();
-        var bmp = slide.GetThumbnail(size);
-        bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-        PdfDocument document = new PdfDocument();
-        //document.Info.Title = "Created with PDFsharp";
+        using (var ms = new MemoryStream())
+        {
+          var bmp = slide.GetThumbnail(size);
+          bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+          PdfDocument document = new PdfDocument();
+          //document.Info.Title = "Created with PDFsharp";
 
-        PdfPage page = document.AddPage();
-        XGraphics gfx = XGraphics.FromPdfPage(page);
+          PdfPage page = document.AddPage();
+          XGraphics gfx = XGraphics.FromPdfPage(page);
 
-        XFont font = new XFont("Verdana", 16);
+          XFont font = new XFont("Verdana", 16);
 
 
-        XImage image = XImage.FromGdiPlusImage(bmp);
+          XImage image = XImage.FromGdiPlusImage(bmp);
 
-        double height = (page.Width * image.PixelHeight) / image.PixelWidth;
 
-        gfx.DrawImage(image, 0, 0, page.Width, height);
+          double width = page.Width - 80;
+          double height = (width * image.PixelHeight) / image.PixelWidth;
 
-        //gfx.DrawImage(image, (dx - width) / 2, (dy - height) / 2, width, height);
-        //      new XRect(0, height+40, page.Width, page.Height),
+          gfx.DrawImage(image, 40, 0, width, height);
 
-        var rect = new XRect(40, height + 40, page.Width - 40, page.Height - (height + 40));
-        XTextFormatter tf = new XTextFormatter(gfx);
-        tf.DrawString(note, font, XBrushes.Black, rect, XStringFormats.TopLeft);
 
-        document.Save("images/" + i + ".pdf");
+          var rect = new XRect(40, height + 40, page.Width - 40, page.Height - (height + 40));
+          XTextFormatter tf = new XTextFormatter(gfx);
+          tf.DrawString(note, font, XBrushes.Black, rect, XStringFormats.TopLeft);
 
+          document.Save("images/" + i + ".pdf");
+        }
 
       }
 
